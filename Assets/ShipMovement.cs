6cd4fae5767d334252589292eeb123ac4815c11b
m.lysikov <max.lysickov2013@gmail.com>
2022-06-13 19:39:06 +0300

    Added turns and tilts of the ship

---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    public float speed = 15;
+
+    public float tiltAngle = 45;
+    
+    public CharacterController ch;
+
+    public Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ch = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0);
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter(movement);
+    }
+
+    private void MoveCharacter(Vector3 direction)
+    {
+        ch.Move(direction * speed * Time.deltaTime);
+        ch.transform.rotation = Quaternion.Euler(0, 0, -movement.x * tiltAngle);
+    }
+}
