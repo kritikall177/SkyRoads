75b44a079b9686d4939544e968e0ea61af93da1c
m.lysikov <max.lysickov2013@gmail.com>
2022-06-23 16:19:26 +0300

    Add flame engine

--- Assets/AsteroidLevitation.cs
+++ Assets/AsteroidLevitation.cs
@@ -6,7 +6,7 @@ public class AsteroidLevitation : MonoBehaviour
     [SerializeField] private Transform positionAsteroid;
     private float _durationRotate = 3f;
     private float _durationMove = 1f;
-    private Vector3 _amplitude = new Vector3(0, 0.3f, 0);
+    private Vector3 _amplitude = new Vector3(0, 0.7f, 0);
     
     private void Start()
     {
