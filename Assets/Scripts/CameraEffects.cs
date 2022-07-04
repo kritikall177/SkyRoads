92c9b8b4401ff3786534cfddde24dd5ec3449449
m.lysikov <max.lysickov2013@gmail.com>
2022-07-04 23:50:39 +0300

    Update CameraEffects.cs

--- Assets/Scripts/CameraEffects.cs
+++ Assets/Scripts/CameraEffects.cs
@@ -6,23 +6,41 @@ using UnityEngine;
 public class CameraEffects : MonoBehaviour
 {
     [SerializeField] public Camera _camera;
-    [SerializeField] private float maxFieldOfView = 80f;
-    [SerializeField] private float minFieldOfView = 60f;
-
+    [SerializeField] public ParticleSystem _particleSystem;
+    
+    [SerializeField] private float maxFOV = 80f;
+    [SerializeField] private float minFOV = 60f;
+    
     private void Start()
     {
-        LerpAccelerationSystem.LerpAction += ChangeCameraFieldOfView;
+        //NewBehaviourScript.WhenLerp += CheckAccelerationOnKey;
+        
+        _particleSystem.Stop();
     }
 
-    private void ChangeCameraFieldOfView(bool isPressed, float percentageOfLerp)
+    private void CheckAccelerationOnKey(bool isPressed, float percentageOfLerp)
     {
         if (isPressed)
         {
-            _camera.fieldOfView = Mathf.Lerp(minFieldOfView, maxFieldOfView, percentageOfLerp);
+            if (_camera.fieldOfView < 80)
+            {
+                _camera.fieldOfView = Mathf.Lerp(minFOV, maxFOV, percentageOfLerp);
+            }
+            else if(!_particleSystem.isPlaying)
+            {
+                _particleSystem.Play();
+            }
         }
         else
         {
-            _camera.fieldOfView = Mathf.Lerp(maxFieldOfView, minFieldOfView, percentageOfLerp);
+            if (_camera.fieldOfView > 60)
+            {
+                _camera.fieldOfView = Mathf.Lerp(maxFOV, minFOV, percentageOfLerp);
+            }
+            else if(!_particleSystem.isStopped)
+            {
+                _particleSystem.Stop();
+            }
         }
     }
 }
